b1049c7390ab001e3d8e95f240d0d0e2865d7985
Jeremy Heyer <jheyer159@gmail.com>
2018-01-10 13:24:43 -0500

    Smarter selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,6 @@
-export const email = (state) => 'values' in state.form.login ? state.form.login.values.email : "";
-export const password = (state) => 'values' in state.form.login ? state.form.login.values.password : "";
+export const email = (state) => 'values' in state.form.login ? 
+                                    'email' in state.form.login.values ? 
+                                        state.form.login.values.email : "" : "" ;
+export const password = (state) => 'values' in state.form.login ? 
+                                        'password' in state.form.login.values ? 
+                                            state.form.login.values.password : "" : "";
