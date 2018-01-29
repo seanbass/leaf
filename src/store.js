00c3aff54cb4a5690aaeeafa4b7e7f67055c93ff
seanbass <seandbass@gmail.com>
2018-01-29 10:55:40 -0800

    Added semicolons.

--- src/store.js
+++ src/store.js
@@ -2,13 +2,14 @@ import {
     applyMiddleware,
     compose
   } from 'redux';
-  import { createLogger } from 'redux-logger'
-  import createSagaMiddleware from 'redux-saga'
+
+  import { createLogger } from 'redux-logger';
+  import createSagaMiddleware from 'redux-saga';
 
   import reducer from './redux/reducers';
   import rootSaga from './redux/sagas';
 
-  import Reactotron from 'reactotron-react-native'
+  import Reactotron from 'reactotron-react-native';
 
   const logger = createLogger({
     level : 'log',
